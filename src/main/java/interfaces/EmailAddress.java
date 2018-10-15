9ef654ca023b474a4bf4f1dba4451855d0056a35
Ross Brazuk <ross.brazuk@gmail.com>
2018-10-15 10:18:24 -0400

    copied project files

---
+++ src/main/java/interfaces/EmailAddress.java
@@ -0,0 +1,9 @@
+package interfaces;
+
+public interface EmailAddress {
+    String getEmail();
+    void setEmail(String email);
+    String getFriendlyName();
+    void setFriendlyName(String friendlyName);
+    boolean isValid();
+}
---
+++ src/main/java/models/EmailAddress.java
@@ -0,0 +1,67 @@
+package models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class EmailAddress implements interfaces.EmailAddress {
+
+    private String email;
+    private String friendlyName;
+
+    public EmailAddress(String email) {
+        this.email = email;
+    }
+
+    public EmailAddress(String email, String friendlyName) {
+        this.email = email;
+        this.friendlyName = friendlyName;
+    }
+
+    @Override
+    public String getEmail() {
+        return this.email;
+    }
+
+    @Override
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String getFriendlyName() {
+        return this.friendlyName;
+    }
+
+    @Override
+    public void setFriendlyName(String friendlyName) {
+        this.friendlyName = friendlyName;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isValid() {
+        if (this.email == null || this.email.isEmpty()) {
+            return false;
+        }
+
+        if (!this.email.contains("@")) {
+            return false;
+        }
+
+        if (this.email.length() > 320) {
+            return false;
+        }
+
+        String[] parts = this.email.split("@");
+
+        if (parts[0].trim().length() < 1 || parts[1].trim().length() < 1) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s <%s>", this.friendlyName, this.email);
+    }
+}
