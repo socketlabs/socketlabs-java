e4b4d2e614d26e91dac3d5833269da4c46862165
Ross Brazuk <ross.brazuk@gmail.com>
2018-10-22 13:57:16 -0400

    Added method to get MimeType in Attachment

--- src/main/java/com/socketLabs/models/Attachment.java
+++ src/main/java/com/socketLabs/models/Attachment.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Represents a message attachment in the form of a byte array.
@@ -43,6 +44,8 @@ public class Attachment {
      */
     public Attachment(String filePath) throws IOException {
         this.content = getContentFromFilePath(filePath);
+        String[] splitPath = filePath.split("\\.");
+        this.mimeType = getMimeTypeFromExtension(splitPath[splitPath.length - 1]);
     }
 
     /**
@@ -71,7 +74,6 @@ public class Attachment {
     }
 
     //TODO - Constructor with name, mimeType, and Stream
-
 
     public byte[] getContent() {
         return this.content;
@@ -110,5 +112,62 @@ public class Attachment {
 
     private byte[] getContentFromFilePath(String filepath) throws IOException {
         return Files.readAllBytes(new File(filepath).toPath());
+    }
+
+    private String getMimeTypeFromExtension(String extension) {
+        switch (extension.toLowerCase()) {
+            case "txt":
+            case "ini":
+            case "sln":
+            case "cs":
+            case "js":
+            case "config":
+            case "vb":
+                return "text/plain";
+            case "jpg":
+            case "jpeg":
+                return "image/jpeg";
+            case "bmp":
+                return "image/bmp";
+            case "csv":
+                return "text/csv";
+            case "doc":
+                return "application/msword";
+            case "docx":
+                return "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+            case "gif":
+                return "image/gif";
+            case "html":
+                return "text/html";
+            case "pdf":
+                return "application/pdf";
+            case "png":
+                return "image/png";
+            case "ppt":
+                return "application/vnd.ms-powerpoint";
+            case "pptx":
+                return "";
+            case "xls":
+                return "";
+            case "xlsx":
+                return "";
+            case "xml":
+                return "application/xml";
+            case "zip":
+                return "";
+            case "wav":
+                return "audio/wav";
+            case "eml":
+                return "message/rfc822";
+            case "mp3":
+                return "audio/mpeg";
+            case "mp4":
+                return "video/mp4";
+            case "mov":
+                return "video/quicktime";
+            default:
+                return "application/octet-stream";
+        }
+
     }
 }
