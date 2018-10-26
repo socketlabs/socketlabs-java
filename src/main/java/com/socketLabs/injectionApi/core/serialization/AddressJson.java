7cd11d14b89ea5ce80df636b7b483476d93ef54f
Ross Brazuk <ross.brazuk@gmail.com>
2018-10-26 11:50:29 -0400

    Added comments for AddressJson

--- src/main/java/com/socketLabs/injectionApi/core/serialization/AddressJson.java
+++ src/main/java/com/socketLabs/injectionApi/core/serialization/AddressJson.java
@@ -3,30 +3,63 @@ package com.socketLabs.injectionApi.core.serialization;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Represents an individual email address for a message.
+ * To be serialized into JSON string before sending to the Injection Api.
+ */
 class AddressJson {
 
+    /**
+     * A valid email address
+     */
     @JsonProperty("emailAddress")
     private String email;
 
+    /**
+     * The friendly or display name for the recipient.
+     */
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty("friendlyName")
     private String friendlyName;
 
+    /**
+     * Creates a new instance of the AddressJson class and sets the email address.
+     * @param email A valid email address
+     * @param friendlyName The friendly or display name for the recipient.
+     */
     public AddressJson(String email, String friendlyName) {
         this.email = email;
         this.friendlyName = friendlyName;
     }
 
+    /**
+     * Gets the email for the recipient
+     * @return String
+     */
     public String getEmail() {
         return this.email;
     }
+
+    /**
+     * Sets the email for the recipient
+     * @param value String
+     */
     public void setEmail(String value) {
         this.email = value;
     }
 
+    /**
+     * Gets the friendly name for the recipient
+     * @return String
+     */
     public String getFriendlyName() {
         return this.friendlyName;
     }
+
+    /**
+     * Sets the friendly name for the recipient
+     * @param value String
+     */
     public void setFriendlyName(String value) {
         this.friendlyName = value;
     }
--- src/main/java/com/socketLabs/injectionApi/message/EmailAddress.java
+++ src/main/java/com/socketLabs/injectionApi/message/EmailAddress.java
@@ -1,33 +1,78 @@
 package com.socketLabs.injectionApi.message;
 
 
+/**
+ * Represents an individual email address for a message
+ * 
+ * Using the constructors
+ * @<code>EmailAddress emailAddress = new EmailAddress("recipient@example.com", "Recipient 1");</code>
+ */
 public class EmailAddress {
 
+    /**
+     * A valid email address
+     */
     private String emailAddress;
+
+    /**
+     * The friendly or display name for the recipient.
+     */
     private String friendlyName;
 
+    /**
+     * Creates a new instance of the email address class and sets the email address and the friendly name.
+     * @param emailAddress String
+     */
     public EmailAddress(String emailAddress) {
         this.emailAddress = emailAddress;
     }
+
+    /**
+     * Creates a new instance of the email address class and sets the email address and the friendly name.
+     * @param emailAddress String
+     * @param friendlyName String
+     */
     public EmailAddress(String emailAddress, String friendlyName) {
         this.emailAddress = emailAddress;
         this.friendlyName = friendlyName;
     }
 
+    /**
+     * Gets the email address for the recipient
+     * @return String
+     */
     public String getEmailAddress() {
         return this.emailAddress;
     }
+
+    /**
+     * Sets the email address for the recipient
+     * @param value String
+     */
     public void setEmailAddress(String value) {
         this.emailAddress = value;
     }
 
+    /**
+     * Gets the friendly name for the recipient
+     * @return String
+     */
     public String getFriendlyName() {
         return this.friendlyName;
     }
+
+    /**
+     * Sets the friendly name for the Recipient
+     * @param value String
+     */
     public void setFriendlyName(String value) {
         this.friendlyName = value;
     }
 
+    /**
+     * Determines if the email address is valid
+     * @return boolean - true if valid, false if not
+     */
     public boolean isValid() {
 
         if (this.emailAddress == null || this.emailAddress.isEmpty()) {
@@ -53,6 +98,11 @@ public class EmailAddress {
 
         return true;
     }
+
+    /**
+     * Represents the email address as a string similar to how it would look in an email client.  Useful for debugging.
+     * @return String
+     */
     @Override
     public String toString() {
         if (this.friendlyName == null || this.friendlyName.isEmpty()) {
