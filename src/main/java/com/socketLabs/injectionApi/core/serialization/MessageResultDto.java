033fdf34a9d3c0ba8d79945bd28d4e5354b6063a
dschrenker-socket <david.schrenker@socketlabs.com>
2018-10-26 14:03:26 -0400

    adding comments

--- src/main/java/com/socketLabs/injectionApi/core/serialization/MessageResultDto.java
+++ src/main/java/com/socketLabs/injectionApi/core/serialization/MessageResultDto.java
@@ -6,6 +6,9 @@ import com.socketLabs.injectionApi.AddressResult;
 
 import java.util.List;
 
+/**
+ * Data transfer object representing a message result from the Injection Api.
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class MessageResultDto {
 
@@ -17,27 +20,51 @@ class MessageResultDto {
     @JsonProperty("ErrorCode")
     private String errorCode;
 
-    /** An array of AddressResult objects that contain the status of each address that failed. If no messages failed this array is empty. */
+    /** A List of AddressResult objects that contain the status of each address that failed. If no messages failed this array is empty. */
     @JsonProperty("AddressResults")
     private List<AddressResult> addressResults;
 
+    /**
+     * Get the index of message being sent
+     * @return
+     */
     public int getIndex() {
         return index;
     }
+    /**
+     * Set the index of message being sent
+     * @param value int
+     */
     public void setIndex(int value) {
         this.index = value;
     }
 
+    /**
+     * Get the response ErrorCode
+     * @return String
+     */
     public String getErrorCode() {
         return errorCode;
     }
+    /**
+     * Set response ErrorCode
+     * @param value String
+     */
     public void setErrorCode(String value) {
         this.errorCode = value;
     }
 
+    /**
+     * Get the List of AddressResult objects
+     * @return List<AddressResult>
+     */
     public List<AddressResult> getAddressResults() {
         return addressResults;
     }
+    /**
+     * Set the List of AddressResult objects
+     * @param value List<AddressResult>
+     */
     public void setAddressResults(List<AddressResult> value) {
         this.addressResults = value;
     }
