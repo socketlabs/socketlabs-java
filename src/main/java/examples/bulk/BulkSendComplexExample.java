23d758b1bd0365a6c074da259d10a51e7f20890f
Ross Brazuk <ross.brazuk@gmail.com>
2018-10-26 10:38:04 -0400

    Fixed bug in BulkdSendFromDataSourceWithMerge and added BulkdSendComplexExample

---
+++ src/main/java/examples/bulk/BulkSendComplexExample.java
@@ -0,0 +1,84 @@
+package examples.bulk;
+
+import com.socketLabs.injectionApi.SendResponse;
+import com.socketLabs.injectionApi.SocketLabsClient;
+import com.socketLabs.injectionApi.message.Attachment;
+import com.socketLabs.injectionApi.message.BulkMessage;
+import com.socketLabs.injectionApi.message.BulkRecipient;
+import com.socketLabs.injectionApi.message.EmailAddress;
+import examples.Example;
+import examples.ExampleConfig;
+
+public class BulkSendComplexExample implements Example {
+
+    @Override
+    public SendResponse RunExample() throws Exception {
+        SocketLabsClient client = new SocketLabsClient(ExampleConfig.ServerId, ExampleConfig.ApiKey);
+
+        // Build the message
+        BulkMessage message = new BulkMessage();
+
+        message.addGlobalMergeData("Motto", "When hitting the inbox matters!");
+        message.addGlobalMergeData("Birthday", "unkown");
+        message.addGlobalMergeData("Age", "an unknown number of");
+        message.addGlobalMergeData("UpSell", "BTW:  You are eligible for discount pricing when you upgrade your service!");
+
+        // Add recipients with merge data
+        BulkRecipient recipient1 = new BulkRecipient("recipient1@example.com");
+        recipient1.addMergeData("Birthday", "08/05/1991");
+        recipient1.addMergeData("Age", "27");
+
+        BulkRecipient recipient2 = new BulkRecipient("recipient2@example.com");
+        recipient2.addMergeData("Birthday", "04/12/1984");
+        recipient2.addMergeData("Age", "34");
+        recipient2.addMergeData("UpSell", "");  // This will override the Global Merge-Data for this specific Recipient
+
+        BulkRecipient recipient3 = new BulkRecipient("recipient3@example.com");
+        recipient3.setFriendlyName("Recipient 3");
+
+        // Set other properties as needed
+        message.setSubject("Complex BulkSend Example");
+        message.setFrom(new EmailAddress("from@example.com", "FromMe"));
+        message.setReplyTo(new EmailAddress("replyto@example.com"));
+
+        message.addCustomHeader("testMessageHeader", "I am a message header");
+
+        // Build the content
+        String html =
+          "<html>"
+        + "   <head><title>Complex</title></head>"
+        + "   <body>"
+        + "       <h1>Merged Data</h1>"
+        + "       <p>"
+        + "           Motto = <b>%%Motto%%</b> </br>"
+        + "           Birthday = <b>%%Birthday%%</b> </br>"
+        + "           Age = <b>%%Age%%</b> </br>"
+        + "           UpSell = <b>%%UpSell%%</b> </br>"
+        + "       </p>"
+        + "       </br>"
+        + "       <h1>Example of Merge Usage</h1>"
+        + "       <p>"
+        + "           Our company motto is '<b>%%Motto%%</b>'. </br>"
+        + "           Your birthday is <b>%%Birthday%%</b> and you are <b>%%Age%%</b> years old. </br>"
+        + "           </br>"
+        + "           <b>%%UpSell%%<b>"
+        + "       </p>"
+        + "   </body>"
+        + "</html>";
+
+        message.setHtmlBody(html);
+
+        // Add an attachment with a custom header
+        Attachment attachment = new Attachment("bus.png", "image/png", "src/main/java/examples/img/bus.png");
+        attachment.addCustomHeader("Attachment-Header", "I Am A Bus");
+        message.getAttachments().add(attachment);
+
+        // Send the message
+
+        return null;
+    }
+
+    // Add some global merge-data (These will be applied to all Recipients unless specifically overridden by Recipient level merge-data)
+
+
+}
--- src/main/java/examples/bulk/BulkSendFromDataSourceWithMerge.java
+++ src/main/java/examples/bulk/BulkSendFromDataSourceWithMerge.java
@@ -23,17 +23,19 @@ public class BulkSendFromDataSourceWithMerge implements Example {
 
         // Retrieve data from the datasource
         CustomerRepository repository = new CustomerRepository();
-        List<Customer> customers = new ArrayList<>();
+        List<Customer> customers = repository.getCustomers();
 
         // Build the message
         BulkMessage message = new BulkMessage();
 
-        message.setSubject("Hello %%FirstName");
+        message.setSubject("Hello %%FirstName%%");
         message.setPlainTextBody("Hello %%FirstName%% %%LastName%%. Is your favorite color still %%FavoriteColor%%?");
         message.setFrom(new EmailAddress("from@example.com"));
         message.setReplyTo(new EmailAddress("replyto@example.com"));
 
         // Merge in the customers from the datasource
+        List<BulkRecipient> recipients = new ArrayList<>();
+
         for (Customer customer : customers) {
             BulkRecipient recipient = new BulkRecipient(customer.getEmailAddress(), customer.getFirstName());
             Map<String, String> mergeData = new HashMap<>();
@@ -41,7 +43,11 @@ public class BulkSendFromDataSourceWithMerge implements Example {
             mergeData.put("LastName", customer.getLastName());
             mergeData.put("FavoriteColor", customer.getFavoriteColor());
             recipient.setMergeData(mergeData);
+
+            recipients.add(recipient);
         }
+
+        message.setTo(recipients);
 
         return client.send(message);
     }
