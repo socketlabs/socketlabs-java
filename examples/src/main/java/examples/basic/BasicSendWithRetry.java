74b05789be8f155f6b71e6113e7164b7d774a087
PraneethChandra <praneeth.chandra@socketlabs.com>
2021-03-25 09:28:27 -0700

    added BasicSendWithRetry

--- examples/src/main/java/examples/basic/BasicSendWithRetry.java
+++ examples/src/main/java/examples/basic/BasicSendWithRetry.java
@@ -1,2 +1,21 @@
-package examples.basic;public class BasicSendWithRetry {
+package examples.basic;
+
+import com.socketLabs.injectionApi.SendResponse;
+import com.socketLabs.injectionApi.SocketLabsClient;
+import com.socketLabs.injectionApi.message.BasicMessage;
+import examples.Example;
+import examples.ExampleConfig;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+
+
+public class BasicSendWithRetry implements Example {
+    public SendResponse RunExample () throws Exception {
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 4433));
+        SocketLabsClient client = new SocketLabsClient(ExampleConfig.ServerId, ExampleConfig.ApiKey, proxy);
+        BasicMessage message = new BasicMessage();
+        SendResponse response = client.send(message);
+        return response;
+    }
 }
